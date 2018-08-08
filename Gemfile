abe77507c48593d3dfe4efad04b80986afd2ba6e
eugenius123 <yugene123@gmail.com>
2018-08-07 23:09:51 -0400

    geocoding added

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ gem 'popper_js', '~> 1.11.1'
 gem 'bootstrap', '4.0.0.alpha6'
 gem 'font-awesome-rails'
 gem 'devise'
+gem 'geocoder'
+gem 'figaro'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 2)
   end
@@ -9,8 +9,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-   current_user.places.create(place_params)
+   @place = current_user.places.create(place_params)
+   if @place.valid?
     redirect_to root_path
+   else
+     render :new, status: :unprocessable_entity
+   end
   end
 
   def show
@@ -19,16 +23,27 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
   def edit
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
